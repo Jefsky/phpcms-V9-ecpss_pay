095c6534d62b2d9d20759a84af0ca343f37888ef
Jefsky <hwj3344@hotmail.com>
2016-12-03 11:13:09 +0800

    Upload ecpss.php

---
+++ languages/ecpss.php
@@ -0,0 +1,34 @@
+<?php
+
+ /** 
+     * ecpss.php 
+     *============================== 
+     *汇潮支付语言插件
+     *============================== 
+     * by:Jefsky  Jefsky@live.com
+     * Nov 30, 2016  
+     */  
+
+global $_LANG;
+
+$_LANG['ecpss']      = '汇潮支付';
+$_LANG['ecpss_desc'] = '汇潮支付是国内领先的独立电子支付及清结算企业，专注于为各类行业和企业提供安全、便捷的综合电子支付服务。';
+$_LANG['md5key'] = 'MD5私钥';
+$_LANG['merno'] = '商户号';
+$_LANG['currency'] = '币种，平台设置商户号币种(1:USD美元;2:EUR欧元3:RMB人民币4:GBP英镑5:HKD港币6:JPY日元7:AUD澳元8:NOK挪威克朗11:CAD加拿大央行货币12:SEK瑞典克朗13:DKK丹麦克朗)';
+$_LANG['language'] = '支付页面的语言 始终是与浏览器的语言模式保持一致（语言  1 中文 2英语  3法语  4西班牙语 5德语）';
+$_LANG['firstname'] = '信用卡持卡人名称';
+$_LANG['remark'] = '备注';
+$_LANG['lastname'] = '信用卡持卡人姓氏';
+$_LANG['email'] = '信用卡持卡人邮箱';
+$_LANG['phone'] = '信用卡持卡人手机';
+$_LANG['zipcode'] = '信用卡持卡人邮政编码';
+$_LANG['address'] = '信用卡持卡人发货地址';
+$_LANG['city'] = '信用卡持卡人城市';
+$_LANG['state'] = '信用卡持卡人州';
+$_LANG['country'] = '信用卡持卡人国家';
+
+
+
+ 
+?>
